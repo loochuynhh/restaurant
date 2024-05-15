c815a77ffd9b88262fd56d713abd5dd8164814f0
nvanhuy1710 <nvanhuy1710@gmail.com>
2024-05-15 10:10:02 +0700

    add base.html

--- RestaurantManagement/settings.py
+++ RestaurantManagement/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/templates/static/'
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'templates/static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
