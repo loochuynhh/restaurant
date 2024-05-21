46f6f7d4c198a054594750d6bf5c344cd009ee81
nvanhuy1710 <nvanhuy1710@gmail.com>
2024-05-21 11:46:38 +0700

    update index

--- reservation/admin.py
+++ reservation/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from reservation.models import Reservation
+from reservation.models import Payment
 
 # Register your models here.
 class ReservationAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Reservation._meta.fields]
     
 admin.site.register(Reservation, ReservationAdmin)
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Payment._meta.fields]
+    
+admin.site.register(Payment, PaymentAdmin)
